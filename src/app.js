2aba9009463bcb255fc384725b0e6ae8d326a8d3
ydalmy <ydalmi.arvelaez@gmail.com>
2025-04-13 15:52:17 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,44 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let caracteres = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let pintas = ["♦","♥", "♠", "♣"];
+let clases =["pinta-roja", "pinta-negra"];
+
+// Generando un número aleatorio entre 0 y el tamaño del arreglo caracteres
+let indiceCaracter = Math.floor(Math.random() * caracteres.length);
+
+// Buscando el indice anterior en el arreglo caracteres
+let caracter = caracteres[indiceCaracter];
+
+// Obteniendo del HTML el DIV que tenga el ID caracter
+let caracterHTMLObj = document.getElementById("caracter");
+
+// Asignado el caracter elegido al DIV anterior
+caracterHTMLObj.innerHTML = caracter;
+
+// Asignando la clase
+let indiceClase = Math.floor(Math.random() * clases.length);
+
+// Buscando el indice anterior en el arreglo clases
+let clase = clases[indiceClase];
+
+// Obteniendo del HTML el DIV que tenga el ID card
+let headerHTMLObj = document.getElementById("header");
+let footerHTMLObj = document.getElementById("footer");
+
+// Asignando la clase elegida al DIV anterior
+headerHTMLObj.classList.add(clase);
+footerHTMLObj.classList.add(clase);
+
+// Asignando la pinta
+let indicePinta = Math.floor(Math.random() * pintas.length);
+
+// Buscando el indice anterior en el arreglo pintas
+let pinta = pintas[indicePinta];
+
+// Asignando la clase elegida al DIV anterior
+footerHTMLObj.innerHTML = pinta;
+headerHTMLObj.innerHTML = pinta;
+
 };
